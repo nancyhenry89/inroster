6c9daa78e25243f30a41bdb9293897b463e9e97e
nancyhenry89 <nancyhenry89@gmail.com>
2018-02-19 12:58:47 +0200

    Compression

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,44 +5,40 @@ $(function() {
         $('nav a').removeClass('active');
         $(this).addClass('active');
         if (isMobile.matches) {
-          $('html, body').animate({
-              scrollTop: $($(this).attr('href')).offset().top-60
-          }, 500, 'linear');
-          $('.navbar-collapse').removeClass('in');
-        }else{
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top - 60
+            }, 500, 'linear');
+            $('.navbar-collapse').removeClass('in');
+        } else {
 
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top-93
-        }, 500, 'linear');
-      }
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top - 93
+            }, 500, 'linear');
+        }
     });
 });
 $(window).scroll(function() {
-if($(this).scrollTop() <= $('#home').offset().top) {
+    if ($(this).scrollTop() <= $('#home').offset().top) {
 
-    $('nav a').removeClass('active');
-  }
-    else if ($(this).scrollTop() < $('#what').offset().top) {
-		    $('nav a').removeClass('active');
+        $('nav a').removeClass('active');
+    } else if ($(this).scrollTop() < $('#what').offset().top) {
+        $('nav a').removeClass('active');
         $('nav a[href="#what"]').addClass('active');
-      }else if($(this).scrollTop() < $('#how').offset().top) {
-		    $('nav a').removeClass('active');
+    } else if ($(this).scrollTop() < $('#how').offset().top) {
+        $('nav a').removeClass('active');
         $('nav a[href="#how"]').addClass('active');
+    } else if ($(this).scrollTop() < $('#features').offset().top) {
+        $('nav a').removeClass('active');
+        $('nav a[href="#features"]').addClass('active');
+    } else if ($(this).scrollTop() < $('#download').offset().top) {
+        $('nav a').removeClass('active');
+        $('nav a[href="#download"]').addClass('active');
+    } else if ($(this).scrollTop() < $('#stats').offset().top) {
+        $('nav a').removeClass('active');
+        $('nav a[href="#stats"]').addClass('active');
+    } else if ($(this).scrollTop() < $('#contact').offset().top) {
+        $('nav a').removeClass('active');
+        $('nav a[href="#contact"]').addClass('active');
     }
-    else if($(this).scrollTop() < $('#features').offset().top) {
-      $('nav a').removeClass('active');
-      $('nav a[href="#features"]').addClass('active');
-  }else if($(this).scrollTop() < $('#download').offset().top) {
-    $('nav a').removeClass('active');
-    $('nav a[href="#download"]').addClass('active');
-}
-else if($(this).scrollTop() < $('#stats').offset().top) {
-  $('nav a').removeClass('active');
-  $('nav a[href="#stats"]').addClass('active');
-}
-else if($(this).scrollTop() < $('#contact').offset().top) {
-  $('nav a').removeClass('active');
-  $('nav a[href="#contact"]').addClass('active');
-}
 
 });
